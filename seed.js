58a87935f784cf5faefd4fa2bf6a693e8363d8e1
middtown <aarontm9@gmail.com>
2018-01-16 17:20:20 -0700

    wireframe images

--- seed.js
+++ seed.js
@@ -1,52 +1,59 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
-var express = require('express');
+var express = require("express");
 var app = express();
-var db = require('./models');
+var db = require("./models");
 
-var newVideogames = [
+var newProjects = [
 {
-	title: "Mass Effect 2",
-	console: "Xbox One & Playstation Pro",
-	year: "2017",
-	multipler: true
+  cover: [String],
+  title: String,
+  name: String,
+  field: String
 },
 {
-	title: "Red Dead Redemption",
-	console: "Xbox One & Playstation Pro",
-	year: "2018",
-	multipler: true
-},
-{
-	title: "Far Cry 5",
-	console: "Xbox One & Playstation Pro",
-	year: "2018",
-	multipler: true
-},
-{
-	title: "Fallout 4",
-	console: "Xbox One & Playstation Pro",
-	year: "2015",
-	multipler: false
-},
-{
-	title: "Kingdom Hearts 3",
-	console: "Playstation 4",
-	year: "2017",
-	multipler: false
+  cover: [String],
+  title: String,
+  name: String,
+  field: String
+},{
+  cover: [String],
+  title: String,
+  name: String,
+  field: String
+},{
+  cover: [String],
+  title: String,
+  name: String,
+  field: String
+},{
+  cover: [String],
+  title: String,
+  name: String,
+  field: String
+},{
+  cover: [String],
+  title: String,
+  name: String,
+  field: String
+},{
+  cover: [String],
+  title: String,
+  name: String,
+  field: String
 }
 ];
 
 
 //removing all videogames to replace games
-db.Videogames.remove({}, function(err, videogames){
+db.Projects.remove({}, function(err, project){
 	if(err){
-		console.log('Error:', err);}
+		console.log("Error:", err);}
 	else{
-		console.log("games seeded");
-		db.Videogames.create(newVideogames, function(err, videogames){
+		console.log("projects seeded");
+		db.Projects.create(newProjects, function(err, projects){
 			if(err){
-				console.log('Error:', err);}
+				console.log("Error:", err);}
 			process.exit();
 		});
 	}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // SERVER-SIDE JAVASCRIPT
 const mongoose = require("mongoose");
+const methodOverride = require("method-override"); 
 const path = require("path");
 const passport = require("passport");
 const flash = require("connect-flash");
@@ -8,39 +9,69 @@ const session = require("express-session");
 const request = require("request");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt-nodejs"); 
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-let behanceKey = require('./env.js');//key and clientId 
+let behanceKey = require("./env.js");//key and clientId 
 let apiUrl = "http://www.behance.net/v2/projects?client_id=" + behanceKey;
 
 // serve static files from public folder
 app.use(express.static(__dirname + "/public"));
+
 //Set up EJS -- look at those views
 app.set("views", __dirname + "/public");
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
+
+                              /************ Passport login begin *******************/
+
+mongoose.connect("mongodb://localhost/local-authentication-with-passport"); 
+
+app.use(morgan("dev")); 
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({ secret: "WDI-GENERAL-ASSEMBLY-EXPRESS" })); 
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(flash());
+
+require("./config/passport")(passport);
+  
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+var routes = require("./config/routes");
+app.use(routes);
+
+                              /************ Passport login end *******************/
+
 
 /************
  * DATABASE *
  ************/
 
 
+
 /**********
  * MODELS *
  **********/
 
-let Schema = mongoose.Schema;
-let ProjectSchema = new Schema({
+// project model and schema
+var Schema = mongoose.Schema;
+
+var ProjectSchema = new Schema({
+    cover: [String],
     title: String,
     name: String,
-    url: String,
-    fields: [String],
-    covers: [String],
-    owners: [String]//check to see if i can go this deep
+    field: String
 });
-let Project = mongoose.model('Project', ProjectSchema);
+
+var Project = mongoose.model("Project", ProjectSchema);
 
 /**************
  * MIDDLEWARE *
@@ -51,30 +82,53 @@ function getProjects(req, res){
     let projectInfo = JSON.parse(body);
     for (var i = 0; i < projectInfo.projects.length; i++) {
       console.log(projectInfo.projects[i].name);
+      res.send(projectInfo.projects[i]);
     }
-      console.log(i);
-      res.send(projectInfo.projects);
-
-      });
-       // console.log( projectInfo.projects[0]);      
-}
-
-function getProjectsByGenre(req, res){
-  let genreReq = "design";
-
-  request(apiUrl+"&q=+"+genreReq, function (error, response, body) {
-    let projectInfoByGenre = JSON.parse(body);
-        console.log(projectInfoByGenre.projects[1]);      
-        res.send(projectInfoByGenre);
   });
 }
+
+function getProjectsByIdea(req, res){
+  let idea = "design";
+  request(apiUrl+"&q=+"+idea, function (error, response, body) {
+    let projectInfoByGenre = JSON.parse(body);
+      for (var i = 0; i < projectInfoByGenre.projects.length; i++) {
+        //console.log(projectInfoByGenre.projects[results].url);    
+        res.send(projectInfo.projects);
+      } 
+  });
+}
+
+// function getProjectsByCity(req, res){
+//   let city = "denver";
+//   request(apiUrl+"&city="+city, function (error, response, body) {
+//     let projectInfoByCity = JSON.parse(body);
+//       for (var i = 0; i < projectInfoByCity.projects.length; i++) {
+//         console.log(projectInfoByCity.projects[i].url);    
+//         res.send(projectInfoByCity.projects);
+//       } 
+//   });
+// }
+
+// function getProjectsByTag(req, res){
+//   let tag = "sketches";
+//   request(apiUrl+"&tags="+tag, function (error, response, body) {
+//     let projectInfoByTag = JSON.parse(body);
+//       for (var i = 0; i < projectInfoByTag.projects.length; i++) {
+//         console.log(projectInfoByTag.projects[i].url);    
+//         res.send(projectInfoByTag.projects);
+//       } 
+//   });
+// }
 
 
 /**********
  * ROUTES *
  **********/
 app.get("/api/projects", getProjects);
-app.get("/api/projects/genre", getProjectsByGenre);
+app.get("/api/projects/idea", getProjectsByIdea);
+
+// app.get("/api/projects/city", getProjectsByCity);
+// app.get("/api/projects/tag", getProjectsByTag);
 
 
 
